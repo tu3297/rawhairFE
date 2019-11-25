d3d3b8c2e02c51dd6e44a75f22cd458a6af58a9d
tu3297 <tu3297@gmail.com>
2019-11-25 08:53:39 +0700

    update product

---
+++ src/components/product/components/AddProduct.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import { Upload, Icon, Modal } from 'antd';
+class AddProduct extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div className="container">
+                <div className="row"></div>
+                <div className="row"></div>
+            </div>
+        )
+    }
+
+}
+export default AddProduct;
